d25de74e393f191dcb8f34620f5a9b4f29f5559b
Mouhamadou Tidiane El Bachir Diop <diopisemou@gmail.com>
2016-11-27 11:40:48 +0100

    Last Commit
    Dashboard issues still not fixed
    Infinte Loop problem
    Registration ang login part done

--- public/includes/js/controllers/projectscontroller.js
+++ public/includes/js/controllers/projectscontroller.js
@@ -1,107 +1,45 @@
 
+dashboardapp.controller("projectscontroller", function($scope) {
 
-devsworldapp.controller("dashbcontrller", function($scope) {
+    var projectList = this;
+    projectList.projects = projects;
 
-    $scope.initFirebase = function() {
-
-    // init UI components
-    this.signInButton = document.getElementById('lgnbtn');
-    this.signUpButton = document.getElementById('sgnbtn');
-    this.signOutButton = document.getElementById('lgtbtn');
-    this.userPic = document.getElementById('user-pic');
-  this.userName = document.getElementById('user-name');
-  // Shortcuts to Firebase SDK features.
-  this.auth = firebase.auth();
-  this.database = firebase.database();
-  this.storage = firebase.storage();
-  // Initiates Firebase auth and listen to auth state changes.
-  this.auth.onAuthStateChanged($scope.onAuthStateChanged.bind(this));
-};
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-$scope.onAuthStateChanged = function(user) {
-  if (user) { // User is signed in!
-   
-   
-
-    // Show  sign-out button.
-   this.signOutButton.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    this.signInButton.setAttribute('hidden', 'true');
-    this.signUpButton.setAttribute('hidden', 'true');
-
-
-
- // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL;
-    var userName = user.displayName;
-
-     // Set the user's profile pic and name.
-    this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
-    this.userName.textContent = userName;
-
-    // We load currently existing chant messages.
-    //this.loadMessages();
-
-    // Show user's profile 
-     this.userName.removeAttribute('hidden');
-    this.userPic.removeAttribute('hidden');
-
-
-     //$state.go('/')
-  }
-   else
-    { // User is signed out!
-
-    
-    // Hide user's profile 
-    this.userName.setAttribute('hidden', 'true');
-    this.userPic.setAttribute('hidden', 'true');
-    
-    
-    // Hide sign-out button.
-    
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-    this.signUpButton.removeAttribute('hidden');
-  }
-
-
-};
-
-// Returns true if user is signed-in. Otherwise false and displays a message.
-$scope.checkSignedInWithMessage = function() {
-  // Return true if the user is signed in Firebase
-  if (this.auth.currentUser) {
-    return true;
-  }
-   
-}
-
-// Signs-out of Dev's World.
-$scope.signOut = function() {
-  // Sign out of Firebase.
-  $scope.checkSetup();
-  $scope.initFirebase();
-  this.auth.signOut();
-};
-
-$scope.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
-  } else if (config.storageBucket === '') {
-    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
-        'actually a Firebase bug that occurs rarely. ' +
-        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
-        'and make sure the storageBucket attribute is not empty. ' +
-        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
-        'displayed there.');
-  }
-};
- 
-   
+       
 });
+
+dashboardapp.controller("NewProjectController", function ($location, projects) {
+
+    var editProject = this;
+    editProject.save = function () {
+        projects.$add(editProject.project).then(function (data) {
+            $location.path('/');
+        });
+    };
+
+});
+
+dashboardapp.controller("EditProjectController", function ($location, $routeParams, projects) {
+
+    var editProject = this;
+    var projectId = $routeParams.projectId,
+        projectIndex;
+
+    editProject.projects = projects;
+    projectIndex = editProject.projects.$indexFor(projectId);
+    editProject.project = editProject.projects[projectIndex];
+
+    editProject.destroy = function () {
+        editProject.projects.$remove(editProject.project).then(function (data) {
+            $location.path('/');
+        });
+    };
+
+    editProject.save = function () {
+        editProject.projects.$save(editProject.project).then(function (data) {
+            $location.path('/');
+        });
+    };
+
+});
+
+
--- public/includes/js/modules/dashboard.js
+++ public/includes/js/modules/dashboard.js
@@ -1,13 +1,81 @@
-var dashboardapp = angular.module("dashboardapp", ["ngRoute"]);
-dashboardapp.config(function($routeProvider) {
+var dashboardapp = angular.module("dashboardapp", ["ngRoute", "firebase"]).value('fbURL', 'https://devfest-6a58a.firebaseio.com/');
+
+
+dashboardapp.service('fbRef', function (fbURL) {
+    return new Firebase(fbURL)
+});
+
+
+dashboardapp.service('fbAuth', function ($q, $firebase, $firebaseAuth, fbRef) {
+    var auth;
+    return function () {
+        if (auth) return $q.when(auth);
+        var authObj = $firebaseAuth(fbRef);
+        if (authObj.$getAuth()) {
+            return $q.when(auth = authObj.$getAuth());
+        }
+        var deferred = $q.defer();
+        authObj.$authAnonymously().then(function (authData) {
+            auth = authData;
+            deferred.resolve(authData);
+        });
+        return deferred.promise;
+    }
+});
+
+dashboardapp.service('Projects', function ($q, $firebase, fbRef, fbAuth, projectListValue) {
+    var self = this;
+    this.fetch = function () {
+        if (this.projects) return $q.when(this.projects);
+        return fbAuth().then(function (auth) {
+            var deferred = $q.defer();
+            var ref = fbRef.child('projects-fresh/' + auth.auth.uid);
+            var $projects = $firebase(ref);
+            ref.on('value', function (snapshot) {
+                if (snapshot.val() === null) {
+                    $projects.$set(projectListValue);
+                }
+                self.projects = $projects.$asArray();
+                deferred.resolve(self.projects);
+            });
+
+            //Remove projects list when no longer needed.
+            ref.onDisconnect().remove();
+            return deferred.promise;
+        });
+    };
+});
+
+dashboardapp.config(function ($routeProvider) {
+
+    var resolveProjects = {
+        projects: function (Projects) {
+            return Projects.fetch();
+        }
+    };
+
     $routeProvider
-    .when("/dashboard", {
+    .when("/", {
         templateUrl : "includes/views/dashboard.htm",
         controller : "dashbcontroller"
     })
-    .when("/projects", {
+        .when("/projects", {
         templateUrl : "includes/views/projects.htm",
         controller : "projectscontroller"
-    })
-    .otherwise({redirectTo : '/dashboard'});
+        })
+        .when('/edit/:projectId', {
+            controller: 'EditProjectController as editProject',
+            templateUrl: 'detail.html',
+            resolve: resolveProjects
+        })
+        .when('/new', {
+        controller: 'NewProjectController as editProject',
+        templateUrl: 'detail.html',
+        resolve: resolveProjects
+        })
+        .when("/friends", {
+            templateUrl: "includes/views/friends.htm",
+            controller: "friendscontroller"
+        })
+        .otherwise({redirectTo : '/'});
 });
